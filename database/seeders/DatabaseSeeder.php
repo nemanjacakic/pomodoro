fcfa02b08b7981c0bad6951917d3ebf043e9723f
Nemanja Cakic <nemanja.c@live.com>
2020-10-15 20:15:27 +0200

    Seed timer sounds as a part of main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,38 +22,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'nemanja.c@live.com'
         ]);
 
-        // Timer::factory()->create([
-        //     'user_id' => $user->id,
-        //     'order' => 0,
-        //     'name' => 'Pomodoro',
-        //     'duration' => 1500
-        // ]);
-
-        // Timer::factory()->create([
-        //     'user_id' => $user->id,
-        //     'order' => 1,
-        //     'name' => 'Short break',
-        //     'duration' => 300
-        // ]);
-
-        // Timer::factory()->create([
-        //     'user_id' => $user->id,
-        //     'order' => 2,
-        //     'name' => 'Long break',
-        //     'duration' => 900
-        // ]);
-
-        // $timerSettings = [
-        //     "timerSound" => true
-        // ];
-
-        // foreach ($timerSettings as $setting => $value) {
-        //     Settings::factory()->create([
-        //         'user_id' => $user->id,
-        //         'key' => $setting,
-        //         'value' => $value,
-        //         'label' => 'timer'
-        //     ]);
-        // }
+        $this->call(TimerSoundSeeder::class);
     }
 }
